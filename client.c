3f7d213bf83c2af07f87f8f192fc4869aa30c8ac
yonshin <yonshin@student.42seoul.kr>
2022-11-16 20:49:57 +0900

    test

--- client.c
+++ client.c
@@ -3,14 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: yonshin <yonshin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 09:07:44 by yonshin           #+#    #+#             */
-/*   Updated: 2022/11/16 09:10:34 by yonshin          ###   ########.fr       */
+/*   Updated: 2022/11/16 20:40:01 by yonshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void	signal_char(int pid, unsigned char c)
+{
+	int	bit = 8;
+	while (bit--) {
+		int signal = SIGUSR1 + (c & 128) / 128;
+		printf("%d %x\n", signal, c);
+		kill(pid, signal);
+		c = c << 1;
+		usleep(1000);
+	}
+}
+
+int	atoi(char *str)
+{
+	int	i = 0;
+	while (*str)
+		i = i * 10 + *str++ - '0';
+	return i;
+}
+
 int	main(int argc, char *argv[])
 {
-	argc++;
+	if (argc < 3)
+		return (0);
+	int		pid = atoi(argv[1]);
+	char	*str = argv[2];
+
+	while (*str)
+		signal_char(pid, *str++);
+	return (0);
 }
--- server.c
+++ server.c
@@ -3,16 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: yonshin <yonshin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 09:07:47 by yonshin           #+#    #+#             */
-/*   Updated: 2022/11/16 09:10:55 by yonshin          ###   ########.fr       */
+/*   Updated: 2022/11/16 20:42:42 by yonshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+void	signal_test(int sig)
+{
+	static int		buff;
+
+	if (buff == 0)
+		buff = 1;
+	buff = buff << 1;
+	if (sig == SIGUSR2)
+		buff += 1;
+	if (buff < 256)
+		return ;
+	buff -= 256;
+	write(1, &buff, 1);
+	buff = 0;
+}
+
 int	main(void)
 {
-	int	a;
-
-	a = 0;
+	printf("%d\n", getpid());
+	signal(SIGUSR1, signal_test);
+	signal(SIGUSR2, signal_test);
+	while (1) ;
 }
